c3802c3f53427e829d54cbc7e33da7cb463c759f
Arondriessen <arondriessen@yahoo.com>
2021-08-25 18:25:19 +0100

    - Added new icons for displaying active puzzle mechanics for each level. - Added a pre-game info screen.

--- ui.js
+++ ui.js
@@ -1098,7 +1098,7 @@ ui = [
           /* 00 */ 1, // state (0 = off, 1 = on)
           /* 01 */ function() { if (uiHover2 != elementID) { animateUIElement([[uiData[2][0][3][1], 9], [uiData[2][0][3][1], 12]], [btFill, btStr], [btFillHov, btStrHov], btHovInSpd, 0); } menuState = 1; }, // onHoverIn event (0 = no event)
           /* 02 */ function() { animateUIElement([[uiData[2][0][3][1], 9], [uiData[2][0][3][1], 12]], [btFillHov, btStrHov], [btFill, btStr], btHovOutSpd, 0); }, // onHoverOut event (0 = no event)
-          /* 03 */ function() { prepareLevelData(); } // onClick event (0 = no event)
+          /* 03 */ function() { prepareLevelData(); gameState = 1; } // onClick event (0 = no event)
         ],
 
         [ // /* 04 */ Image
@@ -1204,7 +1204,7 @@ ui = [
           /* 00 */ 1, // state (0 = off, 1 = on)
           /* 01 */ function() { if (uiHover2 != elementID) { animateUIElement([[uiData[2][0][5][1], 9], [uiData[2][0][5][1], 12]], [btFill, btStr], [btFillHov, btStrHov], btHovInSpd, 0); } menuState = 1; }, // onHoverIn event (0 = no event)
           /* 02 */ function() { animateUIElement([[uiData[2][0][5][1], 9], [uiData[2][0][5][1], 12]], [btFillHov, btStrHov], [btFill, btStr], btHovOutSpd, 0); }, // onHoverOut event (0 = no event)
-          /* 03 */ function() { designMode = !designMode; if (designMode) { solved = 0; } else { if (steps < 1) { solved = -1; } } } // onClick event (0 = no event)
+          /* 03 */ function() { designMode = !designMode; if (designMode) { gameState = 1; } else { if (steps < 1) { gameState = 3; } } } // onClick event (0 = no event)
         ]
       ],
 
@@ -1270,10 +1270,10 @@ ui = [
 
           /* 00 */ 1, // state (0 = off, 1 = on)
           /* 01 */ 1, // type (1 = rect, 2 = rounded rect, 3 = circle)
-          /* 02 */ function() { return ((width / 2) - (bgTileSize * uiScale * 2) + (100)); }, // x pos
-          /* 03 */ function() { return ((height / 2) - (bgTileSize * uiScale) + (70)); }, // y pos
-          /* 04 */ 150, // width
-          /* 05 */ 20, // height
+          /* 02 */ function() { return ((width / 2) - (bgTileSize * uiScale * 2) + (136 * uiScale)); }, // x pos
+          /* 03 */ function() { return ((height / 2) - (bgTileSize * uiScale) + (96 * uiScale)); }, // y pos
+          /* 04 */ 180 * uiScale, // width
+          /* 05 */ 34 * uiScale, // height
           /* 06 */ 0, // h-align (0 = left, 0.5 = centre, 1 = right)
           /* 07 */ 0, // v-align (0 = top, 0.5 = centre, 1 = bottom)
           /* 08 */ 255, // colour
@@ -1291,15 +1291,15 @@ ui = [
           /* 17 */ function() { return 4; }, // v-number (min, dynamically adjusted)
           /* 18 */ function() { return 0; }, // x-offset
           /* 19 */ function() { return 0; }, // y-offset
-          /* 20 */ function() { return 32 * uiScale; }, // x-offset (new line)
-          /* 21 */ function() { return 32 * uiScale; } // y-offset (new line)
+          /* 20 */ function() { return 50 * uiScale; }, // x-offset (new line)
+          /* 21 */ function() { return -50 * uiScale; } // y-offset (new line)
         ],
 
         [ // /* 02 */ Text
 
-          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 00 */ 0, // state (0 = off, 1 = on)
           /* 01 */ function() { return gameMechanicNames[(v * hNum) + h]; }, // text
-          /* 02 */ 24 * uiScale, // text-size
+          /* 02 */ 20 * uiScale, // text-size
           /* 03 */ 255, // text-colour
           /* 04 */ function() { return (30 + (levelData[world - 1][level - 1][5][(v * hNum) + h] * 90)); }, // text-alpha
           /* 05 */ 0, // text-h-align
@@ -1316,11 +1316,11 @@ ui = [
 
         [ // /* 04 */ Image
 
-          /* 00 */ 1, // state (0 = off, 1 = on)
-          /* 01 */ function() { if (levelData[world - 1][level - 1][5][(v * hNum) + h]) { return checkmarkCircle; } else { return xCircle } }, // image
-          /* 02 */ 24 * uiScale, // image-size
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ function() { if (levelData[world - 1][level - 1][5][(v * hNum) + h]) { return gameMechanicIcons[(v * hNum) + h]; } else { return xCircle } }, // image
+          /* 02 */ 60 * uiScale, // image-size
           /* 03 */ 0, // image-alpha
-          /* 04 */ 1, // image-h-align
+          /* 04 */ 0, // image-h-align
           /* 05 */ 0.5, // image-v-align
         ]
       ]
@@ -1328,7 +1328,7 @@ ui = [
 
     [ // /* 01 */ Level Completed
 
-      /* 00 */ function() { return (solved == 1); }, // state (0 = off, 1 = on)
+      /* 00 */ function() { return (gameState == 2); }, // state (0 = off, 1 = on)
 
       [ // /* 01 */ Next Button
 
@@ -1788,7 +1788,7 @@ ui = [
 
     [ // /* 03 */ Row/Column Shifting
 
-      /* 00 */ function() { return (levelData[world - 1][level - 1][5][2] * (solved != 1)); }, // state (0 = off, 1 = on)
+      /* 00 */ function() { return (levelData[world - 1][level - 1][5][2] * (gameState == 1)); }, // state (0 = off, 1 = on)
 
       [ // /* 01 */ Shift Row/Column (Top Left)
 
@@ -2045,7 +2045,7 @@ ui = [
 
     [ // /* 04 */ Corner Tiles Rotation
 
-      /* 00 */ function() { return (levelData[world - 1][level - 1][5][3]  * (solved != 1)); }, // state (0 = off, 1 = on)
+      /* 00 */ function() { return (levelData[world - 1][level - 1][5][3]  * (gameState == 1)); }, // state (0 = off, 1 = on)
 
       [ // /* 01 */ Rotate Corner Tiles (Top)
 
@@ -2471,6 +2471,276 @@ ui = [
         ]
       ]
     ],
+
+    [ // /* 05 */ Pre-Game Info Screen
+
+      /* 00 */ function() { return (gameState == 0); }, // state (0 = off, 1 = on)
+
+      [ // /* 01 */ BG
+
+        1, // /* 00 */ Type (1 = single, 2 = list)
+
+        [ // /* 01 */ Box
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 4, // type (1 = rect, 2 = rounded rect, 3 = circle)
+          /* 02 */ width / 2, // x pos
+          /* 03 */ height / 2, // y pos
+          /* 04 */ bgTileSize * uiScale, // width
+          /* 05 */ bgTileSize * uiScale, // height
+          /* 06 */ 0.5, // h-align (0 = left, 0.5 = centre, 1 = right)
+          /* 07 */ 0.5, // v-align (0 = top, 0.5 = centre, 1 = bottom)
+          /* 08 */ 0, // colour
+          /* 09 */ 5, // alpha
+          /* 10 */ 0, // outline (0 = no, 1 = yes)
+          /* 11 */ 0, // outline-colour
+          /* 12 */ 0, // outline-alpha
+          /* 13 */ 0, // outline-width
+          /* 14 */ 0 // rot-angle
+        ],
+
+        [ // /* 02 */ Text
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 0, // text
+          /* 02 */ 0, // text-size
+          /* 03 */ 0, // text-colour
+          /* 04 */ 0, // text-alpha
+          /* 05 */ 0, // text-h-align
+          /* 06 */ 0 // text-v-align
+        ],
+
+        [ // /* 03 */ Button Events
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 0, // onHoverIn event (0 = no event)
+          /* 02 */ 0, // onHoverOut event (0 = no event)
+          /* 03 */ 0 // onClick event (0 = no event)
+        ],
+
+        [ // /* 04 */ Image
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ function() { return checkMarkIcon; }, // image
+          /* 02 */ 180 * uiScale, // image-size
+          /* 03 */ 40, // image-alpha
+          /* 04 */ 0.5, // image-h-align
+          /* 05 */ 0.5, // image-v-align
+        ]
+      ],
+
+      [ // /* 02 */ BG
+
+        1, // /* 00 */ Type (1 = single, 2 = list)
+
+        [ // /* 01 */ Box
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ 4, // type (1 = rect, 2 = rounded rect, 3 = circle)
+          /* 02 */ width / 2, // x pos
+          /* 03 */ height / 2, // y pos
+          /* 04 */ bgTileSize * uiScale, // width
+          /* 05 */ bgTileSize * uiScale, // height
+          /* 06 */ 0.5, // h-align (0 = left, 0.5 = centre, 1 = right)
+          /* 07 */ 0.5, // v-align (0 = top, 0.5 = centre, 1 = bottom)
+          /* 08 */ 255, // colour
+          /* 09 */ 5, // alpha
+          /* 10 */ 1, // outline (0 = no, 1 = yes)
+          /* 11 */ 255, // outline-colour
+          /* 12 */ 25, // outline-alpha
+          /* 13 */ btStrWidth, // outline-width
+          /* 14 */ 0 // rot-angle
+        ],
+
+        [ // /* 02 */ Text
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 0, // text
+          /* 02 */ 0, // text-size
+          /* 03 */ 0, // text-colour
+          /* 04 */ 0, // text-alpha
+          /* 05 */ 0, // text-h-align
+          /* 06 */ 0 // text-v-align
+        ],
+
+        [ // /* 03 */ Button Events
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 0, // onHoverIn event (0 = no event)
+          /* 02 */ 0, // onHoverOut event (0 = no event)
+          /* 03 */ 0 // onClick event (0 = no event)
+        ],
+
+        [ // /* 04 */ Image
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ function() { return checkMarkIcon; }, // image
+          /* 02 */ 180 * uiScale, // image-size
+          /* 03 */ 40, // image-alpha
+          /* 04 */ 0.5, // image-h-align
+          /* 05 */ 0.5, // image-v-align
+        ]
+      ],
+
+      [ // /* 03 */ World and Level Text
+
+        1, // /* 00 */ Type (1 = single, 2 = list)
+
+        [ // /* 01 */ Box
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ 1, // type (1 = rect, 2 = rounded rect, 3 = circle)
+          /* 02 */ width / 2, // x pos
+          /* 03 */ (height / 2) - ((bgTileSize / 2.3) * uiScale), // y pos
+          /* 04 */ 0, // width
+          /* 05 */ 0, // height
+          /* 06 */ 0, // h-align (0 = left, 0.5 = centre, 1 = right)
+          /* 07 */ 0, // v-align (0 = top, 0.5 = centre, 1 = bottom)
+          /* 08 */ 0, // colour
+          /* 09 */ 0, // alpha
+          /* 10 */ 0, // outline (0 = no, 1 = yes)
+          /* 11 */ 0, // outline-colour
+          /* 12 */ 0, // outline-alpha
+          /* 13 */ 0, // outline-width
+          /* 14 */ 0 // rot-angle
+        ],
+
+        [ // /* 02 */ Text
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ function() { return ("W " + world + "   /   L " + level); }, // text
+          /* 02 */ 42 * uiScale, // text-size
+          /* 03 */ 255, // text-colour
+          /* 04 */ 255, // text-alpha
+          /* 05 */ 0.5, // text-h-align
+          /* 06 */ 0.5 // text-v-align
+        ],
+
+        [ // /* 03 */ Button Events
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 0, // onHoverIn event (0 = no event)
+          /* 02 */ 0, // onHoverOut event (0 = no event)
+          /* 03 */ 0 // onClick event (0 = no event)
+        ],
+
+        [ // /* 04 */ Image
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ function() { return checkMarkIcon; }, // image
+          /* 02 */ 180 * uiScale, // image-size
+          /* 03 */ 40, // image-alpha
+          /* 04 */ 0.5, // image-h-align
+          /* 05 */ 0.5, // image-v-align
+        ]
+      ],
+
+      [ // /* 04 */ Active Mechanics Used in Level
+
+        2, // /* 00 */ Type (1 = single, 2 = list)
+
+        [ // /* 01 */ Box
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ 1, // type (1 = rect, 2 = rounded rect, 3 = circle)
+          /* 02 */ (width / 2) - (12 * uiScale), // x pos
+          /* 03 */ height / 2, // y pos
+          /* 04 */ 280 * uiScale, // width
+          /* 05 */ 44 * uiScale, // height
+          /* 06 */ 0.5, // h-align (0 = left, 0.5 = centre, 1 = right)
+          /* 07 */ 0.5, // v-align (0 = top, 0.5 = centre, 1 = bottom)
+          /* 08 */ 255, // colour
+          /* 09 */ 0, // alpha
+          /* 10 */ 0, // outline (0 = no, 1 = yes)
+          /* 11 */ 0, // outline-colour
+          /* 12 */ 0, // outline-alpha
+          /* 13 */ 0, // outline-width
+          /* 14 */ 0, // rot-angle
+
+          // List Settings
+
+          /* 15 */ function() { return levelData[world - 1][level - 1][5].length; }, // items
+          /* 16 */ function() { return 1; }, // h-number
+          /* 17 */ function() { return 1; }, // v-number (min, dynamically adjusted)
+          /* 18 */ function() { return 0; }, // x-offset
+          /* 19 */ function() { return 0; }, // y-offset
+          /* 20 */ function() { return 0; }, // x-offset (new line)
+          /* 21 */ function() { return 60 * uiScale; } // y-offset (new line)
+        ],
+
+        [ // /* 02 */ Text
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ function() { return gameMechanicNames[(v * hNum) + h]; }, // text
+          /* 02 */ 24 * uiScale, // text-size
+          /* 03 */ 255, // text-colour
+          /* 04 */ function() { return (30 + (levelData[world - 1][level - 1][5][(v * hNum) + h] * 120)); }, // text-alpha
+          /* 05 */ 0, // text-h-align
+          /* 06 */ 0.5 // text-v-align
+        ],
+
+        [ // /* 03 */ Button Events
+
+          /* 00 */ 0, // state (0 = off, 1 = on)
+          /* 01 */ 0, // onHoverIn event (0 = no event)
+          /* 02 */ 0, // onHoverOut event (0 = no event)
+          /* 03 */ 0 // onClick event (0 = no event)
+        ],
+
+        [ // /* 04 */ Image
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ function() { if (levelData[world - 1][level - 1][5][(v * hNum) + h]) { return gameMechanicIcons[(v * hNum) + h]; } else { return xCircle } }, // image
+          /* 02 */ function() { if (levelData[world - 1][level - 1][5][(v * hNum) + h]) { return 56 * uiScale; } else { return 24 * uiScale } }, // image-size
+          /* 03 */ 0, // image-alpha
+          /* 04 */ 1, // image-h-align
+          /* 05 */ 0.5, // image-v-align
+        ]
+      ],
+
+      [ // /* 05 */ Start Button
+
+        1, // /* 00 */ Type (1 = single, 2 = list)
+
+        [ // /* 01 */ Box
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ 4, // type (1 = rect, 2 = rounded rect, 3 = circle)
+          /* 02 */ width / 2, // x pos
+          /* 03 */ (height / 2) + ((bgTileSize - (menuBTMargin)) * uiScale), // y pos
+          /* 04 */ (bgTileSize / 3.2) * uiScale, // width
+          /* 05 */ (bgTileSize / 3.2) * uiScale, // height
+          /* 06 */ 0.5, // h-align (0 = left, 0.5 = centre, 1 = right)
+          /* 07 */ 1, // v-align (0 = top, 0.5 = centre, 1 = bottom)
+          /* 08 */ 255, // colour
+          /* 09 */ 5, // alpha
+          /* 10 */ 0, // outline (0 = no, 1 = yes)
+          /* 11 */ 0, // outline-colour
+          /* 12 */ 0, // outline-alpha
+          /* 13 */ 0, // outline-width
+          /* 14 */ 0 // rot-angle
+        ],
+
+        [ // /* 02 */ Text
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ "S T A R T", // text
+          /* 02 */ 24 * uiScale, // text-size
+          /* 03 */ 255, // text-colour
+          /* 04 */ 255, // text-alpha
+          /* 05 */ 0.5, // text-h-align
+          /* 06 */ 0.5 // text-v-align
+        ],
+
+        [ // /* 03 */ Button Events
+
+          /* 00 */ 1, // state (0 = off, 1 = on)
+          /* 01 */ function() { if (uiHover2 != elementID) { animateUIElement([[uiData[2][5][5][1], 9]], [5], [btFillHov], 6, 0); } menuState = 1; }, // onHoverIn event (0 = no event)
+          /* 02 */ function() { animateUIElement([[uiData[2][5][5][1], 9]], [btFillHov], [5], 6, 0); }, // onHoverOut event (0 = no event)
+          /* 03 */ function() { prepareLevelData(); gameState = 1; } // onClick event (0 = no event)
+        ]
+      ],
+    ]
   ],
 
   [   // /* 03 */ Settings
